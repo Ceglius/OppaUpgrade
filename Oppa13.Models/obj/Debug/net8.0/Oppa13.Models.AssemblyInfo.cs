d530f5c60ac06e80e9727069abb77290c5d3f7b0
DESKTOP-936A7SQ\Tomas-PC <tomas.pateckis@gmail.com>
2025-04-07 10:22:49 +0100

    Ttitulinis page and models import

--- Oppa13.Models/obj/Debug/net8.0/Oppa13.Models.AssemblyInfo.cs
+++ Oppa13.Models/obj/Debug/net8.0/Oppa13.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oppa13.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d872eaee84a97e63ee7a439de91e62dcc34162bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ebeb788742b303801665898f9602ca792084225")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oppa13.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oppa13.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
